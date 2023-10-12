110d955432b3c35f8a85733f08bad8c1d851334d
Ramezannia.M <Ramezannia.M@ronixtools.com>
2023-10-12 09:25:49 +0330

    remove static

--- Mogym/wwwroot/js/theme-customizer/customizer.js
+++
@@ -1,5 +0,0 @@
-if(localStorage.getItem("color"))
-    $("#color" ).attr("href", "../assets/css/"+localStorage.getItem("color")+".css" );
-if(localStorage.getItem("dark"))
-    $("body").attr("class", "dark-only");
-
