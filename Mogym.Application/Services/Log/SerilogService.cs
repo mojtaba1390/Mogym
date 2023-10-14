e35fe15c4e94ebc2f73b132ac154d30fadbd563f
Mojtaba <mojtaba.ramezannia@gmail.com>
2023-10-14 21:12:43 +0330

    add menu list

---
+++ Mogym.Application/Services/Log/SerilogService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mogym.Application.Interfaces.ILog;
+using Mogym.Infrastructure;
+using Mogym.Infrastructure.Interfaces.Log;
+
+namespace Mogym.Application.Services.Log
+{
+    public class SerilogService:ISeriLogService
+    {
+        private IUnitOfWork _unitOfWork;
+        public SerilogService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void LogInformation(string message)
+        {
+            _unitOfWork.SeriLogRepository.LogInformation(message);
+        }
+
+        public void LogWarning(string message)
+        {
+            _unitOfWork.SeriLogRepository.LogWarning(message);
+        }
+
+        public void LogError(string message, Exception exception)
+        {
+            _unitOfWork.SeriLogRepository.LogError(message,exception);
+        }
+    }
+}
