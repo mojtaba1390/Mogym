5b3b694697be221ddfeb7398350ceb37214899d1
Ramezannia.M <Ramezannia.M@ronixtools.com>
2023-11-09 10:58:57 +0330

    user logging log

---
+++ Mogym.Application/Services/Log/UserLoggingService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mogym.Application.Interfaces.ILog;
+using Mogym.Domain.Context;
+using Mogym.Domain.Entities.Log;
+using Mogym.Infrastructure;
+
+namespace Mogym.Application.Services.Log
+{
+    public class UserLoggingService:IUserLoggingService
+    {
+        protected readonly MogymLogContext _context;
+
+        public UserLoggingService(MogymLogContext context)
+        {
+            _context = context;
+        }
+        public async Task Save(string permalink, string ip)
+        {
+            var userLogging = new UserLogging()
+            {
+                Ip = ip,
+                Permalink = permalink
+            };
+           await _context.UserLogging.AddAsync(userLogging);
+        }
+    }
+}
---
+++ Mogym.Domain/Configuration/Log/SeriLogConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Mogym.Domain.Entities.Log;
+
+namespace Mogym.Domain.Configuration.Log
+{
+    public class SeriLogConfiguration : IEntityTypeConfiguration<SeriLog>
+    {
+        public void Configure(EntityTypeBuilder<SeriLog> builder)
+        {
+            builder.ToTable("SeriLog");
+
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Exception)
+                .IsRequired(false);
+
+            builder.Property(p => p.Properties)
+                .IsRequired(false);
+
+            builder.Property(p => p.LogEvent)
+                .IsRequired(false);
+
+            builder.Property(p => p.TimeStamp)
+                .HasColumnType("datetime");
+
+            builder.Property(p => p.Level)
+                .HasMaxLength(16);
+        }
+    }
+}
