ed1d77b7c0ebaa4000da751ac2182d711ace8a6c
Ramezannia.M <Ramezannia.M@ronixtools.com>
2023-11-09 08:50:01 +0330

    userrole record

--- Mogym.Application/AutoMapper/UserRole/User_CreateTrainerUserRoleRecord.cs
+++ Mogym.Application/AutoMapper/UserRole/User_CreateTrainerUserRoleRecord.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Mogym.Application.AutoMapper.UserRole
 {
-    internal class User_UserRoleRecord
+    internal class User_CreateTrainerUserRoleRecord
     {
     }
 }
--- Mogym.Application/Records/UserRole/CreateTrainerUserRoleRecord.cs
+++ Mogym.Application/Records/UserRole/CreateTrainerUserRoleRecord.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Mogym.Application.Records.UserRole
 {
-    internal class CreateUserRoleRecord
+    public class CreateTrainerUserRoleRecord
     {
     }
 }
