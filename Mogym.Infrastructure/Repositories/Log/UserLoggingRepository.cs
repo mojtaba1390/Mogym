91c158e35cc50539e48c18dfac7c149fc52c746a
Mojtaba <mojtaba.ramezannia@gmail.com>
2023-11-12 22:17:37 +0330

    nothing

---
+++ Mogym.Infrastructure/Repositories/Log/UserLoggingRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mogym.Domain.Entities.Log;
+using Mogym.Infrastructure.Interfaces.ILog;
+
+namespace Mogym.Infrastructure.Repositories.Log
+{
+    public class UserLoggingRepository:IUserLoggingRepository
+    {
+        public async Task Save(UserLogging userLogging)
+        {
+            var t = 0;
+        }
+    }
+}
