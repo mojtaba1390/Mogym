255171a268c17ed994e0a552591db9073ccb8d26
Mojtaba <mojtaba.ramezannia@gmail.com>
2023-11-20 20:57:22 +0330

    upload exercise video

--- Mogym.Application/AutoMapper/ExerciseVideo/ExerciseVideo_ExerciseVideoRecord.cs
+++ Mogym.Application/AutoMapper/ExerciseVideo/ExerciseVideo_ExerciseVideoRecord.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Mogym.Application.Records.Exercise;
+using Mogym.Application.Records.ExerciseVideo;
 
 namespace Mogym.Application.AutoMapper.ExerciseVideo
 {
@@ -11,7 +12,7 @@ namespace Mogym.Application.AutoMapper.ExerciseVideo
     {
         public ExerciseVideo_ExerciseVideoRecord()
         {
-            CreateMap<Domain.Entities.ExerciseVideo, ExerciseRecord>().ReverseMap();
+            CreateMap<Domain.Entities.ExerciseVideo, ExerciseVideoRecord>().ReverseMap();
         }
     }
 }
