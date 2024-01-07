661d0ecb8e8c8241e4c3b9965ec121093fc64ec6
Mojtaba <mojtaba.ramezannia@gmail.com>
2024-01-07 22:11:04 +0330

    resolve some issues

--- Mogym.Application/Records/User/LoginRecord.cs
+++ Mogym.Application/Records/User/LoginRecord.cs
@@ -9,7 +9,7 @@ namespace Mogym.Application.Records.User
     public record LoginRecord
     {
         public string? Mobile { get; init; }
-        public string Email { get; init; }
+        public string? Email { get; init; }
         public string Password { get; init; }
         public string? CofirmCode { get; init; }
         public string? ReturnUrl { get; init; }
